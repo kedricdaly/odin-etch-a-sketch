910155fcc3dd9e2dfb32fb364deb6a74827d6a0f
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2023-01-25 15:49:45 -0500

    feat: set up initial cell grid

---
+++ script.js
@@ -0,0 +1,31 @@
+function generateGrid(N) {
+
+
+    const easel = document.querySelector('.easel');
+    // loop twice to generate an NxN grid
+    for (let i = 0; i < N; i++) {
+        // create a grid-row
+        // append cells to grid row, then afterward append grid-row to easel
+        const gridRow = document.createElement('div');
+        gridRow.setAttribute('class','grid-row');
+        for (let j = 0; j < N; j++) {
+            const div = document.createElement('div');
+            div.setAttribute('class','cell');
+            gridRow.appendChild(div);
+        }
+        easel.appendChild(gridRow);
+
+    // add event listeners to the cell elements so when a new grid size
+    // is requested, all cells function appropriately
+    cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => cell.addEventListener('mouseenter', cellHover));
+
+}
+}
+
+function cellHover(e) {
+    console.log(e.target)
+}
+
+window.addEventListener('load',generateGrid(16));
+
